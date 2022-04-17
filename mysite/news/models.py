fb09e60f9f545111454fbbcec86c19341eb256ba
1Demiurg <petrov.alexander.00@bk.ru>
2022-04-17 17:12:32 +0500

    reloading my site from gitignore

---
+++ mysite/news/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+from django.urls import reverse
+
+class News(models.Model):
+    title = models.CharField(max_length=150, verbose_name='Наименования')
+    content = models.TextField(blank=True, verbose_name='Контент')
+    create_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата публикации')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Обновлено')
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', verbose_name='Фото', blank=True, default='default.jpg')
+    is_published = models.BooleanField(default=True, verbose_name='Опубликовано')
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True, verbose_name='Категория')
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('view_news', kwargs={"news_id": self.pk})
+
+    class Meta:
+        verbose_name = 'Новость'
+        verbose_name_plural = 'Новости'
+        ordering = ['-create_at']
+
+
+class Category(models.Model):
+
+    title = models.CharField(max_length=150, db_index=True, verbose_name='Наименования Категории')
+
+    def get_absolute_url(self):
+        return reverse('category', kwargs={"category_id": self.pk})
+
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Категории'
+        verbose_name_plural = 'Категории'
+        ordering = ['title']
