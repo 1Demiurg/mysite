432cc7531b89b40c988d1161a77d1148f2b2d48d
1Demiurg <petrov.alexander.00@bk.ru>
2022-05-05 22:09:49 +0500

    added authorization

--- mysite/news/urls.py
+++ mysite/news/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from .views import *
 urlpatterns = [
     path('register/', register, name='register'),
-    path('login/', login, name='login'),
+    path('login/', user_login, name='login'),
+    path('logout/', user_logout, name='logout'),
 
     path('', HomeNews.as_view(), name='home'),
     path('category/<int:category_id>/', NewsByCategory.as_view(), name='category'),
